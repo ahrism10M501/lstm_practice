406548ebec7c3b2375b6721a4c3d9974fbf61c00
ahrism10m501 <l.all.means@gmail.com>
2025-07-18 17:20:48 +0900

    complete_LSTM

--- model.py
+++ model.py
@@ -2,26 +2,26 @@ import torch
 import torch.nn as nn
 
 class LSTM_cell(nn.Module):
-    def __init__(self, input_size, hidden_size, bias=True):
+    def __init__(self, input_size_size, hidden_size, bias=True):
+
         super(LSTM_cell, self).__init__()
-        self.input_size = input_size
+        _size = input_size_size
         self.hidden_size = hidden_size
         self.bias = bias
 
-        self.x2hf = nn.Linear(input_size, hidden_size, bias=bias)
+        self.x2hf = nn.Linear(input_size_size, hidden_size, bias=bias)
         self.h2hf =  nn.Linear(hidden_size, hidden_size, bias=bias)
 
-        self.x2hi = nn.Linear(input_size, hidden_size, bias=bias)
+        self.x2hi = nn.Linear(input_size_size, hidden_size, bias=bias)
         self.h2hi =  nn.Linear(hidden_size, hidden_size, bias=bias)
 
-        self.x2ho = nn.Linear(input_size, hidden_size, bias=bias)
+        self.x2ho = nn.Linear(input_size_size, hidden_size, bias=bias)
         self.h2ho =  nn.Linear(hidden_size, hidden_size, bias=bias)
 
-        self.x2hc = nn.Linear(input_size, hidden_size, bias=bias)
+        self.x2hc = nn.Linear(input_size_size, hidden_size, bias=bias)
         self.h2hc =  nn.Linear(hidden_size, hidden_size, bias=bias)
         
     def forward(self, x, c, h):
-        x = x.view(-1, x.size(1))
         ft = torch.sigmoid(self.x2hf(x)+self.h2hf(h))
         it = torch.sigmoid(self.x2hi(x)+self.h2hi(h))
         ot = torch.sigmoid(self.x2ho(x)+self.h2ho(h))
@@ -31,40 +31,53 @@ class LSTM_cell(nn.Module):
         ht = torch.mul(torch.tanh(ct), ot)
 
         return ct, ht
-    
+
 class LSTM(nn.Module):
-    def __init__(self, seq_len, batch, input, hidden, layer, output,bias=True):
+    def __init__(self, input_size: int,
+                 hidden_size: int,
+                 layer: int,
+                 output: int,
+                 bias: bool=True):
+        
         super(LSTM, self).__init__()
-        self.seq_len = seq_len
-        self.batch = batch
-        self.input = input
-        self.hidden = hidden
+        self.input_size = input_size
+        self.hidden = hidden_size
         self.layer = layer
-        self.output = output
         self.bias = bias
 
+        # 원하는 수만큼 위로 레이어 쌓기
         self.cell_layer = nn.ModuleList()
-        self.cell_layer.append(LSTM_cell(input, hidden, bias))
+        self.cell_layer.append(LSTM_cell(input_size, hidden_size, bias))
         for _ in range(1, self.layer):
             self.cell_layer.append(LSTM_cell(self.hidden, self.hidden, self.bias))
-        self.fc = nn.Linear(hidden, output)
+        self.fc = nn.Linear(hidden_size, output)
 
-    def _init_hidden(self):
-        c = torch.zeros(self.batch, self.hidden)
-        h = torch.zeros(self.batch, self.hidden)
-        return c, h
+    def forward(self, x):
+        # x: [batch, seq_len, input_size_dim] = (4, 10, 64)
+        # 4배치, 단어 10개짜리 문장, 64개 단어에 대해
 
-    def forward(self, x, c, h):
-        cx, hx = c, h
-        # TODO: seq에 따라 시계열 데이터 처리하기
-        # 지금은 one-to-one -> 생성은 또 어케하는걸까
-        for layer in self.cell_layer:
-            cx, hx = layer(x, cx, hx)
+        # 초기 값
+        c0 = torch.zeros(self.layer, x.size(0), self.hidden, device=x.device)
+        h0 = torch.zeros(self.layer, x.size(0), self.hidden, device=x.device)
 
-        x = self.fc(hx)
-        x = torch.softmax(x, dim=1)
+        # 각 layer의 c, h 값을 각자 관리
+        c_list = [c0[i] for i in range(self.layer)]
+        h_list = [h0[i] for i in range(self.layer)]
 
-        self.cx = cx
-        self.hx = hx
+        # seq_len 만큼 LSTM_cell 반복( t를 위해 t-10까지 갔다온다)
 
-        return x, cx, hx
+        outs = []
+        for t in range(x.size(1)):
+            inp = x[:, t, :]
+            for i, cell in enumerate(self.cell_layer):
+                c_list[i], h_list[i] = cell(inp, c_list[i], h_list[i])
+                inp = h_list[i]
+            
+            # 각 단어에 적합한 단어 예측(시계열 순서대로)
+            out = self.fc(inp)
+            outs.append(out)
+
+        # [batch, seq_len, output_dim]
+        outs = torch.stack(outs, dim=1)
+
+        return outs
